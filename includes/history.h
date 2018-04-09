fc493927f0354cdba38a311c6fd10cc5d21a1d90
Alexis Guérin <aguerin@student.42.fr>
2018-04-09 12:34:22 +0200

    Ajout de get_history

--- includes/history.h
+++ includes/history.h
@@ -37,4 +37,10 @@ t_lstag	*search_history_begin(t_lstag *history, char *command);
 char	*search_history_content(t_lstag *history, const char *pattern);
 void	write_history(char *path, t_lstag *history);
 
+/*
+**	sigleton.c
+*/
+
+t_lstag		**get_history(t_lstag **history);
+
 #endif
